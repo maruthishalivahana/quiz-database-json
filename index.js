1479003731e5a892b4e91ebbb46e706983f09df4
maruthi <maruthikummari553@gmail.com>
2025-04-05 12:53:44 +0530

    changed

--- index.js
+++ index.js
@@ -1,25 +1,27 @@
+const cors = require('cors');
+server.use(cors());
 const jsonServer = require('json-server');
 const cors = require('cors');
 const server = jsonServer.create();
 const router = jsonServer.router({
-  cyber: require('./data/cyber.json'),
-  db: require('./data/db.json'),
-  debug: require('./data/debug.json'),
-  js: require('./data/js.json'),
-  ml: require('./data/ml.json'),
-  progamming: require('./data/programming.json'), // watch typo!
-  quiz: require('./data/quiz.json'),
-  science: require('./data/science.json'),
-  sql: require('./data/sql.json'),
-  tech: require('./data/tech.json'),
-  uiux: require('./data/uiux.json')
+    cyber: require('./data/cyber.json'),
+    db: require('./data/db.json'),
+    debug: require('./data/debug.json'),
+    js: require('./data/js.json'),
+    ml: require('./data/ml.json'),
+    progamming: require('./data/progamming.json'), // watch typo!
+    quiz: require('./data/quiz.json'),
+    science: require('./data/science.json'),
+    sql: require('./data/sql.json'),
+    tech: require('./data/tech.json'),
+    uiux: require('./data/uiux.json')
 });
 const middlewares = jsonServer.defaults();
-server.use(cors()); 
+
 server.use(middlewares);
 server.use(router);
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-  console.log(`🚀 JSON Server is running on port ${PORT}`);
+    console.log(`🚀 JSON Server is running on port ${PORT}`);
 });
